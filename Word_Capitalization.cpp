d63896ad1e2928ab101f6fd3522cdfb9f4176e79
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-15 22:53:17 +0200

    Add files via upload

---
+++ Word_Capitalization.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+//problem link :
+// Name        : Word_Capitalization.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status :
+//============================================================================
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+
+using namespace std;
+ char nedded ( char y){
+if(y>='a'&&y<='z'){
+    y=y-'a'+'A';
+            }
+
+return y;
+
+    }
+int main()
+{
+    char value[1001];
+    cin>>value;
+    char x=value[0];
+    value[0]=nedded(x);
+    cout<<value;
+}
