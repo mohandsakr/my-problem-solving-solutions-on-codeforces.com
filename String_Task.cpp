4cbdcb15a434ebf8c4aebe4eb812b1c7424c2ff2
mohand reda sakr <mohandsakr69@gmail.com>
2017-04-16 00:07:21 +0200

    Add files via upload

---
+++ String_Task.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+//problem link :http://codeforces.com/problemset/problem/118/A
+// Name        : String_Task.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status :accepted
+//============================================================================
+
+ #include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	 string x,result="";
+	 cin>>x;
+	 int y=x.length();
+	 for(int i=0;i<y;i++){
+		 x[i]=tolower(x[i]);
+	 }
+	 for(int i=0;i<y;i++){
+		 if(x[i]!='a'&&x[i]!= 'o'&&x[i]!= 'y'&&x[i]!='e'&& x[i]!='u' && x[i]!= 'i'){
+			 result+=".";
+			 result+=x[i];
+		 }
+	 }
+	 cout<<result;
+	return 0;
+}
