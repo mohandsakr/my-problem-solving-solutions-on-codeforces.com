db7c721ee7ad06ee195c07d32304e0e11dd0c13d
mohand reda sakr <mohandsakr69@gmail.com>
2017-07-17 14:25:14 +0200

    Create gravityFlip.cpp

---
+++ gravityFlip.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    for(int  i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    
+    
+    return 0;
+}
