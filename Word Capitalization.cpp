7864eedc90ec039ecba9e488c60e016a45b147f6
mohand reda sakr <mohandsakr69@gmail.com>
2017-07-20 13:06:52 +0200

    Add files via upload

---
+++ Word Capitalization.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+//problem link:http://codeforces.com/contest/281/problem/A
+// Name        : Word.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+//============================================================================
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	 string s;
+	 cin>>s;
+	 s[0]=toupper(s[0]);
+	 cout<<s;
+	return 0;
+}
