0a76a253b3d888dd206f8f2d8ae5216af88f790d
mohand reda sakr <mohandsakr69@gmail.com>
2017-07-28 17:30:15 +0200

    Add files via upload

---
+++ Juicer.cpp
@@ -0,0 +1,39 @@
+//============================================================================
+//problem link:http://codeforces.com/contest/709/problem/A
+// Name        : Juicer.cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibility
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	 long long n,b,d,sum=0,dtimes=0;
+
+	 cin>>n>>b>>d;
+	 for(int i=0;i<n;i++){
+	int x;
+	cin>>x;
+	if(x<=b){
+		sum+=x;
+	}
+	if(sum>d){
+		dtimes+=1;
+		sum=0;
+	}
+
+	 }
+
+
+
+cout<<dtimes;
+
+
+
+
+	return 0;
+}
