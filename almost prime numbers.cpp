e4f3c572c56f99be403d540a294fb6e3f7cdaa78
mohand reda sakr <mohandsakr69@gmail.com>
2017-08-20 00:29:28 +0200

    Add files via upload

---
+++ almost prime numbers.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+//uva/external/105/10539.pdf
+// Name        : almost.cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibity
+// Description : Hello World in C++, Ansi-style
+//status:
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+long long N=1e12+10;
+bool f[N];
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
