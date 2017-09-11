50df4aa3c610cdb1362d127a4f5d0dae0d947986
mohand reda sakr <mohandsakr69@gmail.com>
2017-09-11 15:22:55 +0200

    Add files via upload

---
+++ 4 A. Watermelon.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+//problem link:
+// Name        : 4 A.cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibility
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+//============================================================================
+
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    if( (m%2==0)&&m>2)
+    cout<<"YES";
+    else cout<<"NO ";
+
+    return 0;
+
+}
