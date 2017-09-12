e75feb46321e5e026fd17a406391b596b40477e5
mohand reda sakr <mohandsakr69@gmail.com>
2017-09-12 17:39:45 +0200

    Add files via upload

---
+++ 116A. Tram.cpp
@@ -0,0 +1,31 @@
+//============================================================================
+//problem link:http://codeforces.com/problemset/problem/116/A
+// Name        : .cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibility
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+//============================================================================
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	 int n;
+	 cin>>n;
+	 int capacity=0;
+	 int stillnow=0;
+	 for(int i=0;i<n;i++){
+		 int a,b;
+		 cin>>a>>b;
+		 stillnow-=a;
+		 stillnow+=b;
+		 capacity=max(capacity,stillnow);
+
+	 }
+	 cout<<capacity;
+
+	return 0;
+}
