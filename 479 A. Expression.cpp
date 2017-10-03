05f48b1d638705202ce85982881bd046720a189a
mohand reda sakr <mohandsakr69@gmail.com>
2017-10-04 00:11:59 +0200

    Add files via upload

---
+++ 479 A. Expression.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+//problem link:http://codeforces.com/problemset/problem/479/A.
+// Name        : A. Expression.cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibility
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin>>a>>b>>c;
+	int exp1=a+b+c;
+	int exp2=a*b*c;
+	int exp3=a+b*c;
+	int exp4=a*b+c;
+	int exp5=a*(b+c);
+	int exp6=(a+b)*c;
+	cout<<max(exp1,max(exp2,max(exp3,max(exp4,max(exp5,exp6)))));
+
+
+ 	return 0;
+}
