e488f0efd18fa782b190df93ec73b51a0bf4adf8
mohand reda sakr <mohandsakr69@gmail.com>
2017-11-07 16:05:48 +0200

    Add files via upload

---
+++ 460A. Vasya and Socks.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+//problem link:http://codeforces.com/problemset/problem/460/A
+// Name        : .cpp
+// Author      : mohand sakr
+// Version     :
+// Copyright   : use it under your responsibility
+// Description : Hello World in C++, Ansi-style
+//status:accepted
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,m;
+	cin>>n>>m;
+	int resuult=(n-1)/(m-1);
+	cout<<n+resuult;
+
+
+
+
+	return 0;
+}
