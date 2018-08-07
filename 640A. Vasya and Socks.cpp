32f58e75672a996c506e38ebd66f99bc735df373
mohand reda sakr <mohandsakr69@gmail.com>
2018-08-07 11:52:08 +0200

    Create 640A. Vasya and Socks.cpp

---
+++ 640A. Vasya and Socks.cpp
@@ -0,0 +1,24 @@
+
+//problem link:http://codeforces.com/contest/460/problem/A
+//ststus:acceprted
+//problem name:640A. Vasya and Socks
+//author:moahnd sakr
+#include <iostream>
+using namespace std;
+
+int main() {
+int n,k;
+cin>>n>>k;
+int current=0;
+int lastbuy=0;
+while(n>0){
+	++current;
+	--n;
+	if(current-lastbuy==k){
+		++n;
+		lastbuy=current;
+	}
+}
+cout<<current;
+	return 0;
+}
