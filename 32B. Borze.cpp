37d4015408a19224e1a0c00c56f4f6fe4d73f332
mohand reda sakr <mohandsakr69@gmail.com>
2019-02-18 11:34:47 +0200

    Create 32B. Borze.cpp

---
+++ 32B. Borze.cpp
@@ -0,0 +1,38 @@
+/*
+*problem  name:B. Borze
+*status:accept
+*problem link:http://codeforces.com/problemset/problem/32/B
+*
+*
+*/
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+ string s,s2;
+ cin>>s;
+int len=s.length();
+for(int i=0;i<len;i++)
+
+{
+	if(s[i]=='.'){
+		s2+='0';
+	}
+	if(s[i]=='-'&&s[i+1]=='.'){
+		s2+='1';
+		++i;
+		
+	}
+	if(s[i]=='-'&&s[i+1]=='-'){
+		s2+='2';
+		++i;
+	}
+	
+} 
+ cout<<s2;
+ 
+ 
+ 
+ 	return 0;
+}
