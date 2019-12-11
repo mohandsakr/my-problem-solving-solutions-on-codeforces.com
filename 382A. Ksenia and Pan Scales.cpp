7d5427ecbdabf1155057df6b28d9d176968ca70d
mohand reda sakr <mohandsakr69@gmail.com>
2019-12-11 17:51:08 +0200

    Create 382A. Ksenia and Pan Scales.cpp

---
+++ 382A. Ksenia and Pan Scales.cpp
@@ -0,0 +1,46 @@
+/*
+problem link:http://codeforces.com/contest/382/problem/A
+problem name:A. Ksenia and Pan Scales
+status:accepted
+author :mohand sakr
+
+
+
+*/
+
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+	
+	string s,s2;
+	cin>>s;
+	cin>>s2;
+	string fst=s.substr(0,s.find('|'));
+	string snd=s.substr(s.find('|')+1,s.length());
+	int fstlen=fst.length();
+	int sndlen=snd.length();
+
+	int len2=s2.length();
+	
+	int total=len2+fstlen+sndlen;
+  if(abs(fstlen-sndlen)<=len2&&total%2==0)
+{
+	 
+ fst+=s2.substr(0,(total>>1)-fstlen);
+ snd+=s2.substr((total>>1)-fstlen,(total>>1)-sndlen);
+ 		
+ 
+		cout<<fst<<"|"<<snd<<endl;
+	 
+}
+else {
+	cout<<"Impossible"<<endl;
+}
+	
+
+	
+	return 0;
+}
