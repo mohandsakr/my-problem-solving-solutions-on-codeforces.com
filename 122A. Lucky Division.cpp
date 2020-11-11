4950e96d7adc703827e8c57b393bb54c642da8a8
Vijay Belwal <54481547+VijayBelwal99@users.noreply.github.com>
2020-11-11 08:11:37 +0530

    Update 122A. Lucky Division.cpp

--- 122A. Lucky Division.cpp
+++ 122A. Lucky Division.cpp
@@ -2,7 +2,7 @@
 //problem link:http://codeforces.com/problemset/problem/122/A
 // Name        : .cpp
 // Author      : mohand sakr
-// Version     :
+// Version     :2.0
 // Copyright   : use it under your responsibility
 // Description : Hello World in C++, Ansi-style
 //status:accepted
@@ -27,3 +27,34 @@ else
 	cout<<"NO";
 return 0;
 }
+
+
+// OR :)
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a[]={4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+    int n;cin>>n;
+    int i=0;
+    
+    while(a[i]<=777)
+    {
+        if(n%a[i]==0)
+        {
+            cout<<"YES";
+            return 0;
+        }
+        i++;
+    }
+    cout<<"NO";
+    
+    return 0;
+    
+}
+
+
