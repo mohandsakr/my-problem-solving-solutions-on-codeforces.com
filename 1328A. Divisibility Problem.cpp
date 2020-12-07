5f3a331ff5a62b556ec5800334ac2971d8322e38
mohand reda sakr <mohandsakr69@gmail.com>
2020-12-07 20:32:34 +0200

    Create 1328A. Divisibility Problem.cpp

---
+++ 1328A. Divisibility Problem.cpp
@@ -0,0 +1,40 @@
+/*
+problem link: https://codeforces.com/problemset/problem/1328/A
+problem name:1328A. Divisibility Problem
+status:ACCEPTED
+author :Mohand Sakr.
+*/
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int t;
+cin>>t;
+
+while(t--){
+	
+	int x,y;
+	cin>>x>>y;
+	
+	if(x%y==0) 
+	{
+	cout<<0<<endl;
+	}
+	else {
+	
+	int w=x%y;
+	
+	cout<<y-w<<endl;
+	
+	}
+}
+
+
+	return 0;
+}
