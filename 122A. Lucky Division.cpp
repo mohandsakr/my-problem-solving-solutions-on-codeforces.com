33ef05871d01d163ef1f97e282a52a1c116a4a69
Mohand Reda Sakr <mohandsakr69@gmail.com>
2021-07-22 13:12:08 +0200

    Update 122A. Lucky Division.cpp

--- 122A. Lucky Division.cpp
+++ 122A. Lucky Division.cpp
@@ -29,32 +29,4 @@ return 0;
 }
 
 
-// OR :)
-
-
-#include<bits/stdc++.h>
-
-using namespace std;
-
-int main()
-{
-    int a[]={4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
-    int n;cin>>n;
-    int i=0;
-    
-    while(a[i]<=777)
-    {
-        if(n%a[i]==0)
-        {
-            cout<<"YES";
-            return 0;
-        }
-        i++;
-    }
-    cout<<"NO";
-    
-    return 0;
-    
-}
-
 
