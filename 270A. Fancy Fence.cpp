c3526057d2a86ef1124836150a196ab3aa18c3e4
abhi29jan <abhi29jan@gmail.com>
2022-01-25 23:52:55 +0530

    Minor fixes

--- 270A. Fancy Fence.cpp
+++ 270A. Fancy Fence.cpp
@@ -1,34 +1,23 @@
-
 /*
-
-prblem name:270A. Fancy Fence
-problem link:https://codeforces.com/contest/270/problem/A
-staus:accepted
-author :mohand sakr
-
-
-*/
-
-
+ *prblem name:270A. Fancy Fence
+ *problem link:https://codeforces.com/contest/270/problem/A
+ *staus:accepted
+ *author :mohand sakr
+ */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-
-int t;
-cin>>t;
-while(t--)
-{
-	int x;
-	cin>>x;
-	if(360%(180-x))cout<<"NO\n";
-	else 
-	cout<<"YES\n";
-	
-}
-
-
-
-	return 0;
+  int t;
+  cin >> t;
+  while (t--) {
+    int x;
+    cin >> x;
+    if (360 % (180 - x))
+      cout << "NO\n";
+    else
+      cout << "YES\n";
+  }
+  return 0;
 }
